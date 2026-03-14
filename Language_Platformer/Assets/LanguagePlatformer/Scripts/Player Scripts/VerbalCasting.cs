4da2ef09de27a10c6317fafeb9a9dce35e8ec90f
GregTheEggIsBadAtCoding <gregsherris@gmail.com>
2026-03-14 15:26:13 -0700

    We live in cities, you'll never see on screen. Something owl cat.

--- Language_Platformer/Assets/LanguagePlatformer/Scripts/Player Scripts/VerbalCasting.cs
+++ Language_Platformer/Assets/LanguagePlatformer/Scripts/Player Scripts/VerbalCasting.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
@@ -14,10 +15,25 @@ public class VerbalCasting : MonoBehaviour
         public string command;
         public int componentID;
     }
+    [Serializable] public class SpellInfo
+    {
+        public string spellName;
+        public int[] commandRequirement = new int[3];
+        public Action spellFunction;
+
+        public SpellInfo(string SN, int[] CREQ, Action SF)
+        {
+            spellName = SN;
+            commandRequirement = CREQ;
+            spellFunction = SF;
+        }
+    }
+
+    [SerializeField] private List<SpellInfo> spellList;
     [SerializeField] private List<CastingInfo> commands;
     private Dictionary<string, int> _dCommands;
     [SerializeField] private int[] currentCommands = new int[3];
-    private int placement = 0;
+    private int _placement = 0;
 
     void Start()
     {
@@ -27,8 +43,10 @@ public class VerbalCasting : MonoBehaviour
             _dCommands[cast.command] = cast.componentID;
         }
         currentCommands[2] = currentCommands[1] = currentCommands[0] = -1;
-        // commands
 
+        // commands
+        SpellInfo first = new SpellInfo("Rock n' Roll", new int[] { 2,2,2 }, PureEarth);
+        spellList.Add(first);
         // balls
 
         recognizer = new KeywordRecognizer(_dCommands.Keys.ToArray(), ConfidenceLevel.Low);
@@ -40,20 +58,41 @@ public class VerbalCasting : MonoBehaviour
         int id = -1;
         if (_dCommands.TryGetValue(args.text, out id))
         {
-            currentCommands[placement] = id;
-            placement++;
-            if (placement == 3)
+            currentCommands[_placement] = id;
+            _placement++;
+            if (_placement == 3)
             {
                 print("Elements: " + currentCommands[0] + ", " + currentCommands[1] + ", " + currentCommands[2]);
-                // Replace the print with a function that is connected to an actual command reading list or smthing, prolly like the directory.
+                SpellSearch(); // Spell search
                 currentCommands[2] = currentCommands[1] = currentCommands[0] = -1;
-                placement = 0; // Israel
+                _placement = 0; // Israel
             }
         }
     }
 
-    void Update()
+    void SpellSearch()
     {
-        
+        foreach (SpellInfo spell in spellList)
+        {
+            if (spell.commandRequirement.SequenceEqual(currentCommands))
+            {
+                print("Casting " + spell.spellName);
+                spell.spellFunction();
+            }
+        }
     }
+
+    void PureEarth()
+    {
+        print("Rocky goodness");
+    }
+    void PureWater()
+    {
+        print("Too much water infact");
+    }
+    void PureFire()
+    {
+        print("You need to sound zesty to make this work");
+    }
+
 }
