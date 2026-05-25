5abd6f29b59643b4727cd29b92019587780ac44b
GregTheEggIsBadAtCoding <gregsherris@gmail.com>
2026-05-24 22:43:06 -0700

    Fireball added, doesn't really do anything but I'll add collision to the attack when I set up a base enemy class.

--- Language_Platformer/Assets/LanguagePlatformer/Scripts/Player Scripts/VerbalCasting.cs
+++ Language_Platformer/Assets/LanguagePlatformer/Scripts/Player Scripts/VerbalCasting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Windows.Speech;
 
 public class VerbalCasting : MonoBehaviour
@@ -10,89 +11,49 @@ public class VerbalCasting : MonoBehaviour
     // Drop him in the sewer and make him a chud
     KeywordRecognizer recognizer;
     // Runtime modifiable casting info to be put into the _dCommands (apple-y named since directory commands)
-    [Serializable] public class CastingInfo
-    {
-        public string command;
-        public int componentID;
-    }
-    [Serializable] public class SpellInfo
+    [Serializable] public class Spell
     {
         public string spellName;
-        public int[] commandRequirement = new int[3];
-        public Action spellFunction;
-
-        public SpellInfo(string SN, int[] CREQ, Action SF)
-        {
-            spellName = SN;
-            commandRequirement = CREQ;
-            spellFunction = SF;
-        }
+        public UnityEvent spellFunction;
+        public bool spellActivated;
+        // public int spellCost;
     }
 
-    [SerializeField] private List<SpellInfo> spellList;
-    [SerializeField] private List<CastingInfo> commands;
-    private Dictionary<string, int> _dCommands;
-    [SerializeField] private int[] currentCommands = new int[3];
-    private int _placement = 0;
+    [SerializeField] private List<Spell> spells;
 
     void Start()
     {
-        _dCommands = new Dictionary<string, int>();
-        foreach (CastingInfo cast in commands)
-        {
-            _dCommands[cast.command] = cast.componentID;
-        }
-        currentCommands[2] = currentCommands[1] = currentCommands[0] = -1;
-
         // commands
-        SpellInfo first = new SpellInfo("Rock n' Roll", new int[] { 2,2,2 }, PureEarth);
-        spellList.Add(first);
+        string[] spellNames = spells.Select(s => s.spellName).ToArray();
         // balls
-
-        recognizer = new KeywordRecognizer(_dCommands.Keys.ToArray(), ConfidenceLevel.Low);
+        recognizer = new KeywordRecognizer(spellNames, ConfidenceLevel.Low);
         recognizer.OnPhraseRecognized += OnPhraseRecognized;
         recognizer.Start();
     }
     void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        int id = -1;
-        if (_dCommands.TryGetValue(args.text, out id))
-        {
-            currentCommands[_placement] = id;
-            _placement++;
-            if (_placement == 3)
-            {
-                print("Elements: " + currentCommands[0] + ", " + currentCommands[1] + ", " + currentCommands[2]);
-                SpellSearch(); // Spell search
-                currentCommands[2] = currentCommands[1] = currentCommands[0] = -1;
-                _placement = 0; // Israel
-            }
-        }
+        // searches for a spell using a lamda expression to specifically
+        // compare the names of the spells to the args text
+        // (which is what the player says) then using the function
+        spells.Find(s => s.spellName == args.text).spellFunction.Invoke();
+        // idk why but I felt like I should of explained this one
     }
 
-    void SpellSearch()
-    {
-        foreach (SpellInfo spell in spellList)
-        {
-            if (spell.commandRequirement.SequenceEqual(currentCommands))
-            {
-                print("Casting " + spell.spellName);
-                spell.spellFunction();
-            }
-        }
-    }
+    [Header("Spells and their stuffs")]
+    [SerializeField] private GameObject fireball;
 
-    void PureEarth()
+    public void PureEarth()
     {
         print("Rocky goodness");
     }
-    void PureWater()
+    public void PureWater()
     {
         print("Too much water infact");
     }
-    void PureFire()
+    public void PureFire()
     {
-        print("You need to sound zesty to make this work");
+        GameObject castPos = GameObject.Find("SSP");
+        Instantiate(fireball, castPos.transform.position, castPos.transform.rotation);
     }
 
 }
