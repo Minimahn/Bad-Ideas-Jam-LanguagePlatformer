72c06a4fc9c524ea0afbe07d674d088e375e879d
Minimahn <ace2312ftw@gmail.com>
2026-05-29 17:32:10 -0400

    some tiling type ish

--- Language_Platformer/Assets/LanguagePlatformer/Scripts/Tilemap/TileController.cs
+++ Language_Platformer/Assets/LanguagePlatformer/Scripts/Tilemap/TileController.cs
@@ -8,52 +8,91 @@ using UnityEngine.UIElements;
 
 public class TileController : MonoBehaviour
 {
-    private List<Vector3Int> glowingTilePositions = new List<Vector3Int>(); //save positions of tiles to remember
+    private List<Vector3Int> glowingTiles = new List<Vector3Int>(); //save positions of tiles to remember
+    // dont need list for burning or freezing, bc they apply and instant decay at certain rate
+    private float freezeTime = 5f;
+    private float burnTime = 5f;
+
     public GameObject player;
     private Tilemap physical;
+    private Tilemap special;
+    private Tilemap lighting;
 
     void Start()
     {
         physical = transform.Find("Physical").GetComponent<Tilemap>();
+        special = transform.Find("Special").GetComponent<Tilemap>();
+        lighting = transform.Find("Lighting").GetComponent<Tilemap>();
     }
 
     void Update()
     {
         //go through all checks -- can make this nicer later
 
-        if (glowingTilePositions.Count > 0)
+        if (glowingTiles.Count > 0)
             checkGlowing();
 
     }
 
 
-    private void checkGlowing()
+    private void checkGlowing() //necessary for glowing bc. range persist based
     {
         List<int> spots = new List<int>();
-        for (int i = 0; i < glowingTilePositions.Count; i++)
+        ScriptableTile tile = new ScriptableTile();
+        for (int i = 0; i < glowingTiles.Count; i++)
         {
             // if out of player render dist, remove from list
             // for now... for every player and monster and its spells hierarchy  (spell, to do)
-            float distToPlr = math.abs((glowingTilePositions[i] - player.transform.position).magnitude);
-            if (distToPlr > 4f)
+            float distToPlr = math.abs((glowingTiles[i] - player.transform.position).magnitude);
+            if (distToPlr > 3.2f || !player.GetComponent<PlayerVariables>().getGlow())
             {
-                ScriptableTile tile = (ScriptableTile)physical.GetTile(glowingTilePositions[i]);
-                // I know its glowing if its here
-                tile.SetSprite("base", physical, glowingTilePositions[i]);
+                tile = (ScriptableTile)physical.GetTile(glowingTiles[i]);
+                if (tile != null)
+                    tile.SetSprite("base", physical, glowingTiles[i]);
+                tile = (ScriptableTile)special.GetTile(glowingTiles[i]);
+                if (tile != null)
+                    tile.SetSprite("base", special, glowingTiles[i]);
+                tile = (ScriptableTile)lighting.GetTile(glowingTiles[i]);
+                if (tile != null)
+                    tile.SetSprite("base", lighting, glowingTiles[i]);
                 spots.Add(i);
+
             }
         }
         for (int i = spots.Count - 1; i >= 0; i--)
-            glowingTilePositions.RemoveAt(spots[i]);
+            glowingTiles.RemoveAt(spots[i]);
     }
 
     public void AddToGlow(List<Vector3Int> tiles)
     {
+        ScriptableTile tile = new ScriptableTile();
         foreach (Vector3Int tilepos in tiles)
         {
-            if (!glowingTilePositions.Contains(tilepos)) //tile could already be there, if not add
-                glowingTilePositions.Add(tilepos);
+            
+            if (!glowingTiles.Contains(tilepos)) //tile could already be there, if not add
+            {
+                tile = (ScriptableTile)physical.GetTile(tilepos);
+                if (tile != null)
+                    tile.SetSprite("glow", physical, tilepos);
+                tile = (ScriptableTile)special.GetTile(tilepos);
+                if (tile != null)
+                    tile.SetSprite("glow", special, tilepos);
+                tile = (ScriptableTile)lighting.GetTile(tilepos);
+                if (tile != null)
+                    tile.SetSprite("glow", lighting, tilepos);
+                glowingTiles.Add(tilepos);
+            }
         }
+    }
+
+    public void AddToBurn(List<Vector3Int> tiles)
+    {
+        
+    }
+
+    public void AddToFreeze(List<Vector3Int> tiles)
+    {
+        
     }
 
 
@@ -74,7 +113,7 @@ public class TileController : MonoBehaviour
     {
         List<Vector3Int> tiles = new List<Vector3Int>();
         BoundsInt bounds = new BoundsInt();
-        bounds.SetMinMax(new Vector3Int((int)(pos.x - dist), (int)(pos.y - 1), (int)pos.z),
+        bounds.SetMinMax(new Vector3Int((int)(pos.x - dist - 1), (int)(pos.y - 1), (int)pos.z),
         new Vector3Int((int)(pos.x + dist), (int)(pos.y), (int)pos.z + 1));
 
         foreach (var pt in bounds.allPositionsWithin)
