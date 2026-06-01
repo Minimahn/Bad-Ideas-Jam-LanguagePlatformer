011fa3dfc52cf7eb64047826872bccf0206b0463
GregTheEggIsBadAtCoding <gregsherris@gmail.com>
2026-06-01 02:20:00 -0700

    burnslop

--- Language_Platformer/Assets/LanguagePlatformer/Scripts/Rope Ignite.cs
+++ Language_Platformer/Assets/LanguagePlatformer/Scripts/Rope Ignite.cs
@@ -1,12 +1,44 @@
 using UnityEngine;
+using UnityEngine.AdaptivePerformance;
 
 public class RopeIgnite : MonoBehaviour
 {
+    private Rope papaRope;
+    [SerializeField] private bool igniting = false;
+    private void Start()
+    {
+        papaRope = gameObject.GetComponentInParent<Rope>();
+    }
+    private void Update()
+    {
+        if (igniting)
+        {
+            SpriteRenderer holder = GetComponent<SpriteRenderer>();
+            if (holder.color.r != 0.0f)
+            {
+                holder.color = new Color(Mathf.Clamp(holder.color.r - (papaRope.individualBurnTime * Time.deltaTime), 0, 67), Mathf.Clamp(holder.color.g - (papaRope.individualBurnTime * Time.deltaTime), 0, 67), Mathf.Clamp(holder.color.b - (papaRope.individualBurnTime * Time.deltaTime), 0, 67), holder.color.a);
+                print("Color = " + holder.color);
+                if (holder.color.r <= 0.5f)
+                    papaRope.Ignited(gameObject);
+
+            }
+            else
+            {
+                igniting = false;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Fire>() != null)
         {
-            gameObject.GetComponentInParent<Rope>().Ignited(gameObject);
+            igniting = true;
         }
+    }
+
+    public void Ignite()
+    {
+        igniting = true;
     }
 }
--- Language_Platformer/Assets/LanguagePlatformer/Scripts/Rope.cs
+++ Language_Platformer/Assets/LanguagePlatformer/Scripts/Rope.cs
@@ -6,12 +6,14 @@ public class Rope : MonoBehaviour
 {
     // Not Private Variables (aka (also known as (nom de plume))) public variables
     [Header("Customization")]
-    [SerializeField] float individualBurnTime = 1.0f;
+    [SerializeField] public float individualBurnTime = 1.0f;
 
     // Private Variables
     private GameObject[] ropeSegments;
     private GameObject anchorStart;
     private GameObject anchorEnd;
+    private bool startRemoval = false;
+
     void Start()
     {
         ropeSegments = new GameObject[transform.childCount - 2];
@@ -28,16 +30,39 @@ public class Rope : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RemovalCheck();
     }
 
     public void Ignited(GameObject toBurnFrom)
     {
-        if (ropeSegments.Last<GameObject>() == toBurnFrom && anchorEnd != null)
-            Destroy(anchorEnd);
-        else if (ropeSegments.First<GameObject>() == toBurnFrom)
-            Destroy(anchorStart);
-        
-        Destroy(toBurnFrom);
+        for (int i = 0; i < ropeSegments.Length; i++)
+        {
+            if (toBurnFrom == ropeSegments[i])
+            {
+                if (i-1 >= 0)
+                    ropeSegments[i-1].GetComponent<RopeIgnite>().Ignite();
+                if (i+1 < ropeSegments.Length)
+                    ropeSegments[i+1].GetComponent<RopeIgnite>().Ignite();
+                break;
+            }
+        }
+    }
+    private void RemovalCheck()
+    {
+        foreach (var rope in ropeSegments)
+        {
+            if (rope.gameObject.GetComponent<SpriteRenderer>().color.r != 0.0f)
+                return;
+        }
+
+        foreach (var rope in ropeSegments)
+        {
+            SpriteRenderer holder = rope.GetComponent<SpriteRenderer>();
+            float alpha = holder.color.a;
+            holder.color = new Color(holder.color.r, holder.color.g, holder.color.b, Mathf.Clamp(holder.color.a - Time.deltaTime, 0, 67));
+            if (holder.color.a <= 0.0f)
+                Destroy(gameObject);
+        }
+
     }
 }
