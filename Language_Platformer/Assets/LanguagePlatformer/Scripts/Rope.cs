ac02001add5728b89e3cf5f918bf00f2dd5e0d4d
GregTheEggIsBadAtCoding <gregsherris@gmail.com>
2026-06-01 00:50:08 -0700

    Finish fire burn tmrw

---
+++ Language_Platformer/Assets/LanguagePlatformer/Scripts/Rope.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    // Not Private Variables (aka (also known as (nom de plume))) public variables
+    [Header("Customization")]
+    [SerializeField] float individualBurnTime = 1.0f;
+
+    // Private Variables
+    private GameObject[] ropeSegments;
+    [SerializeField] GameObject ropeStart;
+    [SerializeField] GameObject ropeEnd;
+    void Start()
+    {
+        ropeSegments = new GameObject[transform.childCount - 2];
+        ropeStart = transform.GetChild(0).gameObject;
+        ropeEnd = transform.GetChild(transform.childCount-1).gameObject;
+        for (int i = 1; i < transform.childCount - 1; i++)
+        {
+            ropeSegments[i-1] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Ignited(GameObject toBurnFrom)
+    {
+        if (ropeSegments.Last<GameObject>() == toBurnFrom)
+            Destroy(ropeEnd);
+        else if (ropeSegments.First<GameObject>() == toBurnFrom)
+            Destroy(ropeStart);
+        
+        Destroy(toBurnFrom);
+    }
+}
